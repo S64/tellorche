868621df9841594dabc0fbb3710e18b0a72feb3b
Shuma Yoshioka <s64.stdio@gmail.com>
2018-11-01 00:17:42 +0900

    Exclude m5stack dependency

--- controllers/m5stack/src/main.ino
+++ controllers/m5stack/src/main.ino
@@ -1,6 +1,5 @@
 // NOTICE: This program is Work-In-Progress.
 
-#include <M5Stack.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
@@ -11,88 +10,65 @@ const char LINE_SEPARATOR = '\n';
 const int LOCAL_PORT = 9000;
 
 void setup() {
-    M5.begin();
     Serial.begin(921600);
     Serial.setTimeout(1000UL);
 
-    M5.Lcd.println("Tellorche M5Stack Controller.");
+    Serial.println("msg: Tellorche M5Stack Controller.");
 }
 
 String wifiSsid = "";
 String wifiPassphrase = "";
 
-// char *buff;
-
 WiFiUDP connection;
 
 void loop() {
-    // M5.Lcd.println("Wait command...");
     if (Serial.available() > 0) {
         String line = Serial.readStringUntil(LINE_SEPARATOR);
 
-        M5.Lcd.println("Received command: `" + line + "`.");
+        Serial.println("msg: Received command: `" + line + "`.");
 
         if (WiFi.status() != WL_CONNECTED) {
             if (line.equals("connect")) {
-                M5.Lcd.println("Connect to Wi-Fi...");
+                Serial.println("msg: Connect to Wi-Fi...");
                 WiFi.begin(wifiSsid.c_str(), wifiPassphrase.c_str());
 
                 while (WiFi.status() != WL_CONNECTED) {
-                    M5.Lcd.print(WiFi.status());
+                    Serial.println("msg: " + WiFi.status());
                     delay(100);
                 }
 
-                Serial.println("Wi-Fi connected.");
-                M5.Lcd.println("Wi-Fi connected.");
+                Serial.println("cmd: Wi-Fi connected.");
+                Serial.println("msg: Wi-Fi connected.");
                 connection.begin(LOCAL_PORT);
-
-                //printReceived();
 
                 return;
             } else if (line.substring(0, 10).equals("wifi_ssid ")) {
                 wifiSsid = line.substring(10);
-                M5.Lcd.println("Wi-Fi SSID: `" + wifiSsid + "`.");
+                Serial.println("msg: Wi-Fi SSID: `" + wifiSsid + "`.");
                 return;
             } else if (line.substring(0, 16).equals("wifi_passphrase ")) {
                 wifiPassphrase = line.substring(16);
-                M5.Lcd.println("Wi-Fi Passphrase: [secret].");
+                Serial.println("msg: Wi-Fi Passphrase: [secret].");
                 return;
             } else {
-                M5.Lcd.println("warn: Wi-Fi not connected.");
+                Serial.println("msg: warn: Wi-Fi not connected.");
                 return;
             }
         } else if (line.equals("disconnect") && WiFi.status() == WL_CONNECTED) {
-            M5.Lcd.println("Disconnecting Wi-Fi...");
+            Serial.println("msg: Disconnecting Wi-Fi...");
             WiFi.disconnect(true);
             while (WiFi.status() == WL_CONNECTED) {
-                M5.Lcd.print(WiFi.status());
+                Serial.println("msg: " + WiFi.status());
                 delay(100);
             }
-            M5.Lcd.println("Wi-Fi disconnected.");
+            Serial.println("msg: Wi-Fi disconnected.");
             return;
         }
-        
-        M5.Lcd.println("Send `" + line +"`.");
+
+        Serial.println("msg: Send `" + line +"`.");
 
         connection.beginPacket(TELLO_IP_ADDR.c_str(), TELLO_UDP_PORT);
         connection.print(line);
         connection.endPacket();
-
-        //printReceived();
     }
 }
-
-/*
-void printReceived() {
-    while (true) {
-        int receiveLength = connection.parsePacket();
-        if (receiveLength != 0) {
-            connection.read(buff, receiveLength);
-            String packet = String(buff).substring(0, receiveLength);
-            M5.Lcd.println(packet);
-        } else {
-            break;
-        }
-    }
-}
-*/
