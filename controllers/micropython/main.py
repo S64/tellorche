d059a38d958f5ae2c697ccac0b16ce8a5726a902
Shuma Yoshioka <s64.stdio@gmail.com>
2018-11-08 20:19:57 +0900

    Fix reset logic

--- controllers/micropython/main.py
+++ controllers/micropython/main.py
@@ -8,63 +8,64 @@ import usocket
 TELLO_ADDR = ('192.168.10.1', 8889)
 
 def main():
-    wifi = None
-    wifi_ssid = None
-    wifi_passphrase = None
-    connection = None
-    responseMessage('Tellorche ESP32 Controller.')
-    responseCommand('wakeup.')
-    try:
-        while True:
-            line = readLine()
-            responseDebugMessage('Received line: `' + line +'`.')
-            if isResetCommand(line):
-                if connection is not None:
-                    connection.close()
-                    connection = None
-                if wifi is not None:
-                    responseMessage('Disconnecting Wi-Fi...')
-                    wifi.disconnect()
-                    while wifi.isconnected():
-                        time.sleep(1)
-                        responseDebugMessage('.')
-                    responseMessage('Wi-Fi disconnected.')
-                    responseCommand('Wi-Fi disconnected.')
-                    wifi.active(False)
-                    wifi = None
+    while True:
+        wifi = None
+        wifi_ssid = None
+        wifi_passphrase = None
+        connection = None
+        responseMessage('Tellorche ESP32 Controller.')
+        responseCommand('wakeup.')
+        try:
+            while True:
+                line = readLine()
+                responseDebugMessage('Received line: `' + line +'`.')
+                if isResetCommand(line):
+                    if connection is not None:
+                        connection.close()
+                        connection = None
+                    if wifi is not None:
+                        responseMessage('Disconnecting Wi-Fi...')
+                        wifi.disconnect()
+                        while wifi.isconnected():
+                            time.sleep(1)
+                            responseDebugMessage('.')
+                        responseMessage('Wi-Fi disconnected.')
+                        responseCommand('Wi-Fi disconnected.')
+                        wifi.active(False)
+                        wifi = None
+                    else:
+                        responseMessage('Wi-Fi already disconnected.')
+                    break
+                if isControllerCommand(line):
+                    cmd = sliceControllerCommandBody(line)
+                    if cmd.startswith('wifi_ssid '):
+                        wifi_ssid = cmd[10:]
+                        responseMessage('Wi-Fi SSID: `' + wifi_ssid + '`.')
+                    elif cmd.startswith('wifi_passphrase '):
+                        wifi_passphrase = cmd[16:]
+                        responseMessage('Wi-Fi Passphrase: [secret]')
+                    elif cmd == 'connect':
+                        responseMessage('Connect to Wi-Fi...')
+                        wifi = network.WLAN(network.STA_IF)
+                        wifi.active(True)
+                        wifi.connect(wifi_ssid, wifi_passphrase)
+                        while not wifi.isconnected():
+                            time.sleep(1)
+                            responseDebugMessage('.')
+                        responseMessage('Wi-Fi connected.')
+                        responseCommand('Wi-Fi connected.')
+                        connection = usocket.socket(usocket.AF_INET, usocket.SOCK_DGRAM)
+                    else:
+                        responseMessage('Can\'t understand controller cmd: `' + cmd + '`.')
+                elif isTelloCommand(line):
+                    cmd = sliceTelloCommandBody(line)
+                    responseDebugMessage('Send to tello: `' + cmd + '`.')
+                    sendTelloCommand(connection, cmd)
                 else:
-                    responseMessage('Wi-Fi already disconnected.')
-                break
-            if isControllerCommand(line):
-                cmd = sliceControllerCommandBody(line)
-                if cmd.startswith('wifi_ssid '):
-                    wifi_ssid = cmd[10:]
-                    responseMessage('Wi-Fi SSID: `' + wifi_ssid + '`.')
-                elif cmd.startswith('wifi_passphrase '):
-                    wifi_passphrase = cmd[16:]
-                    responseMessage('Wi-Fi Passphrase: [secret]')
-                elif cmd == 'connect':
-                    responseMessage('Connect to Wi-Fi...')
-                    wifi = network.WLAN(network.STA_IF)
-                    wifi.active(True)
-                    wifi.connect(wifi_ssid, wifi_passphrase)
-                    while not wifi.isconnected():
-                        time.sleep(1)
-                        responseDebugMessage('.')
-                    responseMessage('Wi-Fi connected.')
-                    responseCommand('Wi-Fi connected.')
-                    connection = usocket.socket(usocket.AF_INET, usocket.SOCK_DGRAM)
-                else:
-                    responseMessage('Can\'t understand controller cmd: `' + cmd + '`.')
-            elif isTelloCommand(line):
-                cmd = sliceTelloCommandBody(line)
-                responseDebugMessage('Send to tello: `' + cmd + '`.')
-                sendTelloCommand(connection, cmd)
-            else:
-                responseMessage('Can\'t understand: `' + line + '`.')
-    finally:
-        if wifi is None or wifi.isconnected():
-            responseDebugMessage('Finally block. But Wi-Fi isn\'t disconnected!')
+                    responseMessage('Can\'t understand: `' + line + '`.')
+        finally:
+            if wifi is None or wifi.isconnected():
+                responseDebugMessage('Finally block. But Wi-Fi isn\'t disconnected!')
 
 
 def readLine():
