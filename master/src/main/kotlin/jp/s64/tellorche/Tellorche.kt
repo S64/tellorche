7a2b85361b91024ff808b747b771b4bd7e5fbac6
Shuma Yoshioka <s64.stdio@gmail.com>
2018-11-17 12:24:12 +0900

    Split files

--- master/src/main/kotlin/jp/s64/tellorche/Tellorche.kt
+++ master/src/main/kotlin/jp/s64/tellorche/Tellorche.kt
@@ -20,31 +20,8 @@ import java.io.File
 
 object Tellorche {
 
-    private val moshi = Moshi.Builder()
-            .add(ControllerType.Adapter)
-            .add(TelloCommand.Adapter)
-            .build()
-
-    private lateinit var config: TellorcheConfig
-
-    private lateinit var controllers: Map<ControllerId, ITelloController>
-
-    private var safeEnded: Boolean = false
-
     @JvmStatic
     fun main(orgArgs: Array<String>) {
-
-        Runtime.getRuntime().addShutdownHook(Thread {
-            if (!safeEnded) {
-                System.err.println("Do crash!")
-                controllers.forEach {
-                    it.value.doCrash()
-                }
-            } else {
-                System.out.println("Safe shutdown.")
-            }
-        })
-
         val args = Args()
         val parser = CmdLineParser(args)
 
@@ -56,88 +33,10 @@ object Tellorche {
         }
 
         when (args.mode) {
-            is SequenceMode -> execSequenceMode(args.mode as SequenceMode)
+            is SequenceMode -> SequenceLogic(args.mode as SequenceMode).exec()
         }
     }
 
-    private fun execSequenceMode(args: SequenceMode) {
-        args.let {
-            it.configFile.readText(Charsets.UTF_8)
-                    .let { config = TellorcheConfigJsonAdapter(moshi).fromJson(it) ?: TODO("Parse Error") }
-        }
-
-        controllers = config.controllers.mapValues {
-            it.value.createInterface(it.key)
-        }
-
-        do {
-            println("[Tellorche] Config file loaded. Input `exec` to start sequence:")
-            if (readLine() == "exec") break
-        } while (true)
-
-
-        // exec
-        mainLoop(args.startAtInMillis)
-        safeEnded = true
-
-        controllers.forEach {
-            it.value.dispose()
-        }
-    }
-
-    private fun mainLoop(skip: TimeInMillis?) {
-        val initAt = System.currentTimeMillis()
-        val executedPeriods = mutableSetOf<TimeInMillis>()
-
-        val startTick: TimeInMillis
-
-        if (skip != null) {
-            executedPeriods.addAll(config.sequence.keys.filter { it < skip })
-            startTick = skip
-        } else {
-            startTick = 0
-        }
-
-        do {
-            val tickStartAt = System.currentTimeMillis()
-            val currentTick = (tickStartAt - initAt) + startTick
-            val toExecuteTick = currentTick + config.accuracy
-
-            val targets = config.sequence.keys.filter { it <= toExecuteTick && !executedPeriods.contains(it) }
-
-            if (targets.isNotEmpty()) {
-                println("[Tellorche] Period: ($currentTick)...$toExecuteTick:")
-            }
-
-            targets.forEach { timeInMillis ->
-                val seq = config.sequence[timeInMillis]!!
-
-                seq.forEach { action ->
-                    action.controllers.forEach { controllerId ->
-                        doControl(
-                                controllerId,
-                                action.command,
-                                action.params
-                        )
-                    }
-                }
-
-                executedPeriods.add(timeInMillis)
-            }
-
-            if (executedPeriods.size == config.sequence.size) {
-                println("[Tellorche] All sequences was executed.")
-                break
-            }
-        } while (true)
-    }
-
-    private fun doControl(id: ControllerId, command: TelloCommand, orgParams: List<TelloActionParam>) {
-        val params = TelloCommand.convertParams(command, orgParams, config.scale)
-
-        controllers[id]!!
-                .send(command, params)
-    }
 }
 
 class Args {
