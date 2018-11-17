c4a6eb67fb39f8634f6cc11c1afa93c288bb5d8c
Shuma Yoshioka <s64.stdio@gmail.com>
2018-11-17 12:17:40 +0900

    Split main function to sequence mode

--- master/src/main/kotlin/jp/s64/tellorche/Tellorche.kt
+++ master/src/main/kotlin/jp/s64/tellorche/Tellorche.kt
@@ -9,9 +9,13 @@ import jp.s64.tellorche.entity.TelloCommand
 import jp.s64.tellorche.entity.TellorcheConfig
 import jp.s64.tellorche.entity.TellorcheConfigJsonAdapter
 import jp.s64.tellorche.entity.TimeInMillis
+import org.kohsuke.args4j.Argument
 import org.kohsuke.args4j.CmdLineException
 import org.kohsuke.args4j.CmdLineParser
 import org.kohsuke.args4j.Option
+import org.kohsuke.args4j.spi.SubCommand
+import org.kohsuke.args4j.spi.SubCommandHandler
+import org.kohsuke.args4j.spi.SubCommands
 import java.io.File
 
 object Tellorche {
@@ -51,6 +55,12 @@ object Tellorche {
             throw e
         }
 
+        when (args.mode) {
+            is SequenceMode -> execSequenceMode(args.mode as SequenceMode)
+        }
+    }
+
+    private fun execSequenceMode(args: SequenceMode) {
         args.let {
             it.configFile.readText(Charsets.UTF_8)
                     .let { config = TellorcheConfigJsonAdapter(moshi).fromJson(it) ?: TODO("Parse Error") }
@@ -132,9 +142,22 @@ object Tellorche {
 
 class Args {
 
+    @Argument(handler = SubCommandHandler::class, required = true, index = 0)
+    @SubCommands(
+        SubCommand(name = "sequence", impl = SequenceMode::class)
+    )
+    lateinit var mode: Mode
+
+}
+
+class SequenceMode : Mode() {
+
     @Option(name = "--config", metaVar = "path", required = true)
     lateinit var configFile: File
 
     @Option(name = "--startAt", metaVar = "timeInMillis", required = false)
     var startAtInMillis: TimeInMillis? = null
+
 }
+
+sealed class Mode
