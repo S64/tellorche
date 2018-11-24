e732fc8d37c0ae943eb476923afd27aa5974647b
Shuma Yoshioka <s64.stdio@gmail.com>
2018-11-24 14:34:13 +0900

    Impl validates

--- master/src/main/kotlin/jp/s64/tellorche/Tellorche.kt
+++ master/src/main/kotlin/jp/s64/tellorche/Tellorche.kt
@@ -1,5 +1,10 @@
 package jp.s64.tellorche
 
+import com.squareup.moshi.Moshi
+import jp.s64.tellorche.entity.ControllerType
+import jp.s64.tellorche.entity.TelloCommand
+import jp.s64.tellorche.entity.TellorcheConfig
+import jp.s64.tellorche.entity.TellorcheConfigJsonAdapter
 import jp.s64.tellorche.entity.TimeInMillis
 import org.kohsuke.args4j.Argument
 import org.kohsuke.args4j.CmdLineException
@@ -30,6 +35,18 @@ object Tellorche {
             is ValidateMode -> System.exit(ValidateLogic(args.mode as ValidateMode, out = System.out, err = System.err).exec())
         }
     }
+
+    private val moshi = Moshi.Builder()
+            .add(ControllerType.Adapter)
+            .add(TelloCommand.Adapter)
+            .build()
+
+    fun parseConfig(configFile: File): TellorcheConfig {
+        return TellorcheConfigJsonAdapter(moshi).fromJson(
+                configFile.readText(Charsets.UTF_8)
+        ) ?: TODO("Parse error.")
+    }
+
 }
 
 class Args {
--- master/src/main/kotlin/jp/s64/tellorche/ValidateLogic.kt
+++ master/src/main/kotlin/jp/s64/tellorche/ValidateLogic.kt
@@ -1,5 +1,8 @@
 package jp.s64.tellorche
 
+import com.fazecast.jSerialComm.SerialPort
+import jp.s64.tellorche.entity.ControllerType
+import jp.s64.tellorche.entity.ISerialControllerConfig
 import org.everit.json.schema.Schema
 import org.everit.json.schema.ValidationException
 import org.everit.json.schema.loader.SchemaLoader
@@ -8,7 +11,7 @@ import org.json.JSONTokener
 import java.io.PrintStream
 
 class ValidateLogic(
-        mode: ValidateMode,
+        private val mode: ValidateMode,
         private val out: PrintStream,
         private val err: PrintStream
 ) {
@@ -37,7 +40,50 @@ class ValidateLogic(
             err.println(e.allMessages)
             return EXIT_CODE_ERR
         }
+
+        val parsed = Tellorche.parseConfig(mode.configFile)
+
+        parsed.controllers.forEach { id, controller ->
+            val config: ISerialControllerConfig? = when (controller.type) {
+                ControllerType.MICRO_PYTHON -> controller.microPythonConfigs
+                ControllerType.ESP32 -> controller.esp32Configs
+                else -> null
+            }
+
+            config?.let {
+                val com = SerialPort.getCommPort(it.comPortDescriptor)
+                try {
+                    if (!com.openPort()) {
+                        warn("$id に設定された ${it.comPortDescriptor} は現在開けません")
+                    }
+                } finally {
+                    com.closePort()
+                }
+            }
+        }
+
+        var invalidControllerId = false
+        parsed.sequence.forEach { time, commands ->
+            commands.forEachIndexed { i, action ->
+                if (!parsed.controllers.keys.containsAll(action.controllers)) {
+                    fail("sequence[\"$time\"][$i] で 設定されたcontrollerIdは定義されていません")
+                    invalidControllerId = true
+                }
+            }
+        }
+        if (invalidControllerId) return EXIT_CODE_ERR
+
+        out.println("ok: この設定ファイルは利用できます")
+
         return EXIT_CODE_OK
+    }
+
+    private fun warn(msg: String) {
+        out.println("w: $msg")
+    }
+
+    private fun fail(msg: String) {
+        err.println("e: $msg")
     }
 
 }
